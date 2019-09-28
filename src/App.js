610aeff73423b2c0ab486551b6a91652a811d4ff
Guy Marushchenko <guym@allegro.ai>
2019-09-28 18:58:07 +0300

    added basic chat

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './App.css';
+import Messages from "./Messages";
+import Input from "./Input";
+
+function randomName() {
+  const names = [
+    "Guy","Chuch","Sapir","Gal"]
+  return names[Math.floor(Math.random() * names.length)];;
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+
+class App extends Component {
+  state = {
+    messages: [],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    }
+  }
+
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone("gfzuJb85bihke6iy", {
+      data: this.state.member
+    });
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = {...this.state.member};
+      member.id = this.drone.clientId;
+      this.setState({member});
+    });
+    const room = this.drone.subscribe("observable-room");
+    room.on('data', (data, member) => {
+      const messages = this.state.messages;
+      messages.push({member, text: data});
+      this.setState({messages});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Best chat everrr</h1>
+        </div>
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input
+          onSendMessage={this.onSendMessage}
+        />
+      </div>
+    );
+  }
+
+  onSendMessage = (message) => {
+    this.drone.publish({
+      room: "observable-room",
+      message
+    });
+  }
+
+}
+
+export default App;
