24f3defd33e0cb1a6799708885253f3e6877a92e
Guy Marushchenko <guym@allegro.ai>
2019-10-11 00:27:03 +0300

    Basic chat structure, shows results of an http request to some random endpoint

--- src/App.js
+++ src/App.js
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Messages from "./Messages";
 import Input from "./Input";
 
-function randomName() {
-  const names = [
-    "Guy","Chuch","Sapir","Gal"]
-  return names[Math.floor(Math.random() * names.length)];;
-}
-
-function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
 
 class App extends Component {
-  state = {
-    messages: [],
-    member: {
-      username: randomName(),
-      color: randomColor(),
+    state = {
+        messages: [],
+        member: {
+            username: "Guy",
+            color: "blue",
+        }
     }
-  }
 
-  constructor() {
-    super();
-    this.drone = new window.Scaledrone("gfzuJb85bihke6iy", {
-      data: this.state.member
-    });
-    this.drone.on('open', error => {
-      if (error) {
-        return console.error(error);
-      }
-      const member = {...this.state.member};
-      member.id = this.drone.clientId;
-      this.setState({member});
-    });
-    const room = this.drone.subscribe("observable-room");
-    room.on('data', (data, member) => {
-      const messages = this.state.messages;
-      messages.push({member, text: data});
-      this.setState({messages});
-    });
-  }
+    constructor() {
+        super();
+        var xhr = new XMLHttpRequest();
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Best chat everrr</h1>
-        </div>
-        <Messages
-          messages={this.state.messages}
-          currentMember={this.state.member}
-        />
-        <Input
-          onSendMessage={this.onSendMessage}
-        />
-      </div>
-    );
-  }
+        xhr.addEventListener('load', () => {
+            const member = {...this.state.member};
+            member.id = 1;
+            this.setState({member});
+            const messages = this.state.messages;
+            let response = JSON.parse(xhr.responseText);
+            for (const [key, value] of Object.entries(response)) {
+                let entries = Object.entries(value);
+                for (const [key, value] of entries) {
+                    messages.push({member: member, text: key, father: null, id: null, tag: null, action: null});
 
-  onSendMessage = (message) => {
-    this.drone.publish({
-      room: "observable-room",
-      message
-    });
-  }
+                }
+                messages.push({member: member, text: key, father: null, id: null, tag: null, action: null});
+            }
+            this.setState({messages});
+
+        });
+        xhr.open('GET', 'https://dog.ceo/api/breeds/list/all');
+        xhr.send();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Best chat everrr</h1>
+                </div>
+                <div className="information">
+                    <h2>Conversation insights:</h2>
+                </div>
+                <Messages
+                    messages={this.state.messages}
+                    currentMember={this.state.member}
+                />
+                <Input
+                    onSendMessage={this.onSendMessage}
+                />
+            </div>
+        );
+    }
+
+    onSendMessage = (message) => {
+
+    }
 
 }
 
--- src/Messages.js
+++ src/Messages.js
@@ -6,26 +6,23 @@ class Messages extends Component {
     const {messages} = this.props;
     return (
       <ul className="Messages-list">
-        {messages.map(m => this.renderMessage(m))}
+        {messages.map(m => Messages.renderMessage(m))}
       </ul>
+
     );
   }
 
-  renderMessage(message) {
+  static renderMessage(message) {
     const {member, text} = message;
-    const {currentMember} = this.props;
-    const messageFromMe = member.id === currentMember.id;
-    const className = messageFromMe ?
-      "Messages-message currentMember" : "Messages-message";
     return (
-      <li className={className}>
+      <li className="Messages-message">
       <span
         className="avatar"
-        style={{backgroundColor: member.clientData.color}}
+        style={{backgroundColor: member.color}}
       />
         <div className="Message-content">
           <div className="username">
-            {member.clientData.username}
+            {member.username}
           </div>
           <div className="text">{text}</div>
         </div>
