247decac4bef0fecff0e236350e311e496f6bf54
Guy Marushchenko <guymar@post.bgu.ac.il>
2020-03-25 14:19:12 +0200

    Created discussion author is the current user

--- src/DiscussionPage/Modals/CreateDiscussionModal.js
+++ src/DiscussionPage/Modals/CreateDiscussionModal.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Modal from 'react-bootstrap4-modal';
 import "./CreateDiscussionModal.css"
 import io from "socket.io-client";
@@ -16,11 +17,11 @@ class CreateDiscussionModal extends Component {
         const xhr = new XMLHttpRequest();
         xhr.addEventListener('load', () => {
             let discussion_id = JSON.parse(xhr.responseText)["discussion"];
-            this.addMessage(discussion_id, 0, 'Sapir', description, 0);
+            this.addMessage(discussion_id, 0, this.props.currentUser, description, 0);
         });
         xhr.open('POST', 'http://localhost:5000/api/createDiscussion');
         xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({title: title, categories: []}));
+        xhr.send(JSON.stringify({ title: title, categories: [] }));
         this.updateVisibility(false);
     };
 
@@ -52,13 +53,13 @@ class CreateDiscussionModal extends Component {
                     <div className="modal-body">
                         <p className="title">Title:</p>
                         <input type="text" className="title-input" name="title"
-                               placeholder="Enter Title"/>
+                            placeholder="Enter Title" />
                         <p className="description">Description:</p>
-                        <textarea className="description-input" name="description" placeholder={"Write Something"}/>
+                        <textarea className="description-input" name="description" placeholder={"Write Something"} />
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-grey"
-                                onClick={() => this.updateVisibility(false)}>Cancel
+                            onClick={() => this.updateVisibility(false)}>Cancel
                         </button>
                         <button className="btn btn-info">Create</button>
                     </div>
@@ -68,4 +69,10 @@ class CreateDiscussionModal extends Component {
     }
 }
 
-export default CreateDiscussionModal;
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser
+    };
+};
+
+export default connect(mapStateToProps)(CreateDiscussionModal);
