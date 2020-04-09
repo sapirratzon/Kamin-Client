396a334ebff9cd222087e6e59000f9deaff61956
Guy Marushchenko <guymar@post.bgu.ac.il>
2020-04-09 16:00:18 +0300

    Add vertical lines to the messages so it easier to know their position and some small css improvements

--- src/DiscussionPage/Chat/MessagesList.js
+++ src/DiscussionPage/Chat/MessagesList.js
@@ -5,10 +5,10 @@ import Message from './Message';
 const Messages = (props) => {
     return (
         <ul className="Messages-list">
-            {props.messages.map((m, i) => <React.Fragment>
+            {props.messages.map((m, i) =>
                 <Message key={i} username={m.author} color={m.color} text={m.text} timestamp={m.timestamp}
-                         depth={m.depth} id={m.id} isSimulation={props.isSimulation}
-                         newMessageHandler={props.newMessageHandler} newAlertHandler={props.newAlertHandler}/></React.Fragment>)}
+                    depth={m.depth} id={m.id} isSimulation={props.isSimulation}
+                    newMessageHandler={props.newMessageHandler} newAlertHandler={props.newAlertHandler} />)}
         </ul>
     );
 }
