cedc69fdb5581f7923b2d0951a8788df584b4734
ronelhar <ronelharrar@gmail.com>
2020-04-10 11:33:55 +0300

    fixed discussionStats import

--- src/DiscussionPage/Discussion.js
+++ src/DiscussionPage/Discussion.js
@@ -5,7 +5,7 @@ import Simulation from './Simulation/Simulation';
 import Graph from "./Graph/Graph";
 import AlertList from "./Alert/AlertsList";
 import UserStats from "./Statistics/UserStats";
-import DiscussionStats from "./Statistics/UserStats";
+import DiscussionStats from "./Statistics/DiscussionStats";
 
 class Discussion extends Component {
     constructor(props) {
