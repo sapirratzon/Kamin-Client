f31cfc42505bb69402534d24d90372549b309255
ronelhar <ronelharrar@gmail.com>
2020-04-10 11:12:13 +0300

    fixed alert bug

--- src/DiscussionPage/Alert/Alert.js
+++ src/DiscussionPage/Alert/Alert.js
@@ -1,27 +1,10 @@
 import React from "react";
-import {Card,CardHeader,CardBody,CardTitle} from "reactstrap"
-
+import { Card, CardHeader, CardBody, CardTitle } from "reactstrap";
 const Alert = (props) => {
     return (
-        // <li id="alert">
-        //     <p><i className="fas fa-exclamation-triangle"/>{props.text}</p>
-        // </li>
-        <Card className="card-stats" style={{ border: '3px solid #4285f4' }}>
-            <CardHeader className="p-1">
-                <CardTitle tag="h4">Moderation Alerts</CardTitle>
-            </CardHeader>
-            <CardBody className="p-1">
-                <li id="alert">
-                    <p><i className="fas fa-exclamation-triangle" />{props.text}</p>
-                </li>
-
-            </CardBody>
-            {/* <CardFooter>
-                    <div className="stats">
-                        <i className="now-ui-icons ui-2_time-alarm" /> Last 7 days
-                  </div>
-                </CardFooter> */}
-        </Card>
+        <li id="alert">
+            <p><i className="fas fa-exclamation-triangle" />{props.text}</p>
+        </li>
     );
 }
 
--- src/DiscussionPage/Alert/AlertsList.js
+++ src/DiscussionPage/Alert/AlertsList.js
@@ -4,9 +4,19 @@ import "./Alerts.css";
 
 const AlertList = (props) => {
     return (
-        <ul id="alerts-list" className="mt-2">
-            {props.alerts.map((a, i) => <Alert key={i} text={a.text}/>)}
-        </ul>
+
+        <Card className="card-stats" style={{ border: '3px solid #4285f4' }}>
+            <CardHeader className="p-1">
+                <CardTitle tag="h4">Moderation Alerts</CardTitle>
+            </CardHeader>
+            <CardBody className="p-1">
+                <Container>
+                    <ul id="alerts-list" className="mt-2">
+                        {props.alerts.map((a, i) => <Alert key={i} text={a.text} />)}
+                    </ul>
+                </Container>
+            </CardBody>
+        </Card>
     );
 };
 
