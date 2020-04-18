dfdfe0548fd6fd5ab99d6035793172c9c10baf91
Guy Marushchenko <guymar@post.bgu.ac.il>
2020-04-18 17:00:54 +0300

    Add show more button at long messages

--- src/DiscussionPage/Chat/Message.js
+++ src/DiscussionPage/Chat/Message.js
@@ -13,8 +13,28 @@ class Message extends Component {
             showAlertInput: false,
             alertText: "Alert",
             inputText: "",
+            fullyShown: true,
+            shownText: "",
+            textLengthMessage: "",
+            longMessage: false
         };
     }
+
+    componentDidMount() {
+        if (this.props.text.length > 500) {
+            this.setState({
+                longMessage: true,
+                fullyShown: false,
+                shownText: this.props.text.substring(0, 500),
+                textLengthMessage: " Show more"
+            });
+        } else {
+            this.setState({
+                fullyShown: true,
+                shownText: this.props.text
+            });
+        }
+    };
 
     componentDidUpdate = () => {
         if (this.props.isSimulation) {
@@ -58,7 +78,7 @@ class Message extends Component {
     };
 
     sendMessageHandler = (message) => {
-        if (message.length === 0 ) return;
+        if (message.length === 0) return;
         this.state.showReplyInput ? this.props.newMessageHandler(this.props.id, "Guy", message, this.props.depth + 1) :
             this.props.newAlertHandler(this.props.id, message);
         this.replyHandler();
@@ -76,6 +96,19 @@ class Message extends Component {
         }).format(date);
     };
 
+    handleMessageDisplayLength = () => {
+        this.state.fullyShown ? this.setState({
+            fullyShown: false,
+            shownText: this.props.text.substring(0, 500),
+            textLengthMessage: " Show more"
+        })
+            : this.setState({
+                fullyShown: true,
+                shownText: this.props.text,
+                textLengthMessage: " Show less"
+            });
+    };
+
     render() {
         let depthPixels = this.props.depth * 20;
         let depthString = depthPixels.toString() + "px";
@@ -83,40 +116,43 @@ class Message extends Component {
         for (let i = 0; i < this.props.depth + 1; i++) {
             verticalLines.push(<div className="vl" key={i} style={{
                 "left": ((20 * (i + 1) - depthPixels) + 2) + "px",
-            }}/>)
+            }} />)
         }
         return (
             <React.Fragment>
-                <li className="Messages-message" style={{"marginLeft": depthString}}>
+                <li className="Messages-message" style={{ "marginLeft": depthString }}>
                     {verticalLines}
                     <span
                         className="avatar"
                         style={{
                             "backgroundColor": this.props.color,
                         }}
-                        // onClick={this.props.updateSelectedUser(this.props.username)}
                     />
                     <div className="Message-content">
                         <div className="username">
                             {this.props.username}{"  "}{this.getDate(this.props.timestamp)}
                         </div>
-                        <div className="text">{this.props.text}</div>
+                        <div className="text">
+                            {this.state.shownText}
+                            {this.state.longMessage && <b className="text-primary message-buttons" onClick={this.handleMessageDisplayLength}> {this.state.textLengthMessage} </b>}
+
+                        </div>
                         {!this.props.isSimulation ?
                             <React.Fragment>
                                 <div className="reply">
                                     <p>
-                                        <i className="far fa-comment-dots mr-2"
-                                           onClick={this.replyHandler}>{this.state.replyText}</i>
+                                        <i className="far fa-comment-dots mr-2 message-buttons"
+                                            onClick={this.replyHandler}>{this.state.replyText}</i>
                                         {this.props.userType === 'MODERATOR' || this.props.userType === 'ROOT' ?
-                                            <i className="far fa-bell"
-                                               onClick={this.alertHandler}>{this.state.alertText}</i> : null}
+                                            <i className="far fa-bell message-buttons"
+                                                onClick={this.alertHandler}>{this.state.alertText}</i> : null}
                                     </p>
                                 </div>
                             </React.Fragment>
                             : null
                         }
                         {this.state.showReplyInput || this.state.showAlertInput ?
-                            <Input onSendMessage={this.sendMessageHandler} placeHolder={this.state.inputText}/>
+                            <Input onSendMessage={this.sendMessageHandler} placeHolder={this.state.inputText} />
                             : null
                         }
                     </div>
