89aa9dcefb3a80d4cbe262ea5e8258a04bde99c9
sapirratzon <sapir.ratzon23@gmail.com>
2020-05-12 11:40:37 +0300

    fix bug : default statistics visualization config

--- src/DiscussionPage/Chat/Messages.js
+++ src/DiscussionPage/Chat/Messages.js
@@ -7,16 +7,16 @@ const Messages = (props) => {
 
     return (
         <ul className="Messages-list" >
-            {props.messages.map((m) =>
+            { props.messages.map((m) =>
                 <Message
-                    key={m.id} username={m.author} color={m.color} text={m.text}
-                    timestamp={m.timestamp}
-                    depth={m.depth} id={m.id} isSimulation={props.isSimulation}
-                    newCommentHandler={props.newCommentHandler} newAlertHandler={props.newAlertHandler}
-                    updateAlertedMessage={props.updateAlertedMessage}
-                    updateVisibility={props.updateVisibility}
-                    selected={props.selectedMessage === m.id}
-                />)}
+                    key={ m.id } username={ m.author } color={ m.color } text={ m.text }
+                    timestamp={ m.timestamp }
+                    depth={ m.depth } id={ m.id } isSimulation={ props.isSimulation }
+                    newCommentHandler={ props.newCommentHandler } newAlertHandler={ props.newAlertHandler }
+                    updateAlertedMessage={ props.updateAlertedMessage }
+                    updateVisibility={ props.updateVisibility }
+                    selected={ props.selectedMessage === m.id }
+                />) }
         </ul >
     );
 };
