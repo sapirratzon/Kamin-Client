fb9b220cce2597beac09c1e7b407a6133b33a7b9
Guy Marushchenko <guymar@post.bgu.ac.il>
2020-05-13 12:32:27 +0300

    Change Messages to stateless

--- src/DiscussionPage/Chat/Messages.js
+++ src/DiscussionPage/Chat/Messages.js
@@ -1,43 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import Message from './Message';
 import { connect } from 'react-redux'
 
-class Messages extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            branchId: '~'
-        }
-    }
-
-    render() {
-        return (
-            <ul className="Messages-list" >
-                {this.props.messages.map((m) => {
-                    debugger
-                    let showMessage = true;
-                    let showMoreMessages = false;
-                    for (let collapsedNode of this.props.collapsedNodes) {
-                        if (m.branchId === collapsedNode)
-                            showMoreMessages = true;
-                        else if (m.branchId !== collapsedNode && m.branchId.startsWith(collapsedNode))
-                            showMessage = false;
-                    }
-                    return showMessage && <Message
-                        key={m.id} username={m.author} color={m.color} text={m.text}
-                        timestamp={m.timestamp} directionClass={this.props.directionClass}
-                        depth={m.depth} id={m.id} isSimulation={this.props.isSimulation}
-                        newCommentHandler={this.props.newCommentHandler} newAlertHandler={this.props.newAlertHandler}
-                        updateAlertedMessage={this.props.updateAlertedMessage}
-                        updateVisibility={this.props.updateVisibility}
-                        selected={this.props.selectedMessage === m.id}
-                        branchId={m.branchId}
-                        showMoreMessages={showMoreMessages}
-                    />
-                })}
-            </ul >
-        );
-    }
+const Messages = (props) => {
+    return (
+        <ul className="Messages-list" >
+            {props.messages.map((m) => {
+                debugger
+                let showMessage = true;
+                let showMoreMessages = false;
+                for (let collapsedNode of props.collapsedNodes) {
+                    if (m.branchId === collapsedNode)
+                        showMoreMessages = true;
+                    else if (m.branchId !== collapsedNode && m.branchId.startsWith(collapsedNode))
+                        showMessage = false;
+                }
+                return showMessage && <Message
+                    key={m.id} username={m.author} color={m.color} text={m.text}
+                    timestamp={m.timestamp} directionClass={props.directionClass}
+                    depth={m.depth} id={m.id} isSimulation={props.isSimulation}
+                    newCommentHandler={props.newCommentHandler} newAlertHandler={props.newAlertHandler}
+                    updateAlertedMessage={props.updateAlertedMessage}
+                    updateVisibility={props.updateVisibility}
+                    selected={props.selectedMessage === m.id}
+                    branchId={m.branchId}
+                    showMoreMessages={showMoreMessages}
+                />
+            })}
+        </ul >
+    );
 };
 
 const mapStateToProps = state => {
